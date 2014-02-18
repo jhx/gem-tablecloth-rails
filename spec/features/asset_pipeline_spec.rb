3a3de1909115a03b3d86c1dceab82c8a76d3c715
Doc Walker <doc.walker@jameshardie.com>
2014-02-18 15:26:03 -0600

    Fix rubocop TrailingWhitespace offenses

--- spec/features/asset_pipeline_spec.rb
+++ spec/features/asset_pipeline_spec.rb
@@ -4,21 +4,21 @@ feature 'Asset pipeline' do
   scenario 'Browser requests application javascript' do
     visit asset_path('application.js')
     # save_and_open_page  # for debugging (opens page in browser)
-    
+
     expect(page).to have_text(<<-EOF
-  	Tablecloth 
+  	Tablecloth
   	written by Alen Grakalic, provided by Css Globe (cssglobe.com)
   	please visit http://cssglobe.com/lab/tablecloth/
     EOF
     )
   end # scenario
-  
+
   scenario 'Browser requests application stylesheet' do
     visit asset_path('application.css')
     # save_and_open_page  # for debugging (opens page in browser)
-    
+
     expect(page).to have_text(<<-EOF
-  	TableCloth	
+  	TableCloth
   	by Alen Grakalic, brought to you by cssglobe.com
     EOF
     )
